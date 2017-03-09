792dd9934acdd3138d54d10b3eed96f3fa847031
sagewall <sagemaps@gmail.com>
2017-03-09 13:55:45 -0700

    Add mapbox sdk and dataset functions

--- src/app/edit-map/edit-map.component.ts
+++ src/app/edit-map/edit-map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+import {Component, OnInit} from '@angular/core';
+const MapboxClient = require('mapbox/dist/mapbox-sdk.js');
+const Mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 const MapboxDraw = require('@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.js');
 
 @Component({
@@ -9,20 +10,62 @@ const MapboxDraw = require('@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.js');
 })
 export class EditMapComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
-    const map = new mapboxgl.Map({
+    const datasetId = 'cizym3hr9037a31qlrnpygtoy';
+    const client = new MapboxClient('sk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiY2l6eWxjN2R3MDMxYjJxbmgwOWNyNmJsaiJ9.gARjF95J8MPHt8VoCKnwWQ');
+    Mapboxgl.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
+
+    const map = new Mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/sagewall/ciwf7ja64001o2psg2v73nsya',
       center: [-105.25, 39.75],
       zoom: 10
     });
 
-    const Draw = new MapboxDraw();
+    const draw = new MapboxDraw();
 
-    map.addControl(Draw);
+    map.on('load', () => {
+      map.addControl(draw);
+      client.listFeatures(datasetId, {}, (err, collection) => {
+        if (err) {
+          console.log(err);
+        } else {
+          draw.add(collection);
+        }
+      });
+    });
+
+    map.on('draw.create', (e) => {
+      if (e.hasOwnProperty('features')) {
+        for (const eventFeature of e.features) {
+          client.insertFeature(eventFeature, datasetId, (err, feature) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('inserting feature:');
+              console.log(feature);
+            }
+          });
+        }
+      }
+    });
+
+    map.on('draw.delete', (e) => {
+      if (e.hasOwnProperty('features')) {
+        for (const eventFeature of e.features) {
+          client.deleteFeature(eventFeature.id, datasetId, (err, feature) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('deleting feature');
+              console.log(eventFeature);
+            }
+          });
+        }
+      }
+    });
   }
-
 }
